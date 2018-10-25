17b214f8fb01e3c393358273b3111b1f4d899419
Tucker Wray <tuckerwray84@gmail.com>
2018-10-24 21:21:36 -0400

    update supplementscraper.py> my_url> syntax added ('') to hyperlink.

--- supplementscraper.py
+++ supplementscraper.py
@@ -1,7 +1,7 @@
 import bs4
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
-my_url = https://www.amazon.com/s/ref=nb_sb_ss_c_1_15?url=search-alias%3Daps&field-keywords=tumeric+supreme+gaia&sprefix=tumeric+supreme%2Caps%2C154&crid=11381C0PRK8IO
+my_url = "https://www.amazon.com/s/ref=nb_sb_ss_c_1_15?url=search-alias%3Daps&field-keywords=tumeric+supreme+gaia&sprefix=tumeric+supreme%2Caps%2C154&crid=11381C0PRK8IO"
 
 #makes connection and grabs the webpage
 uClient = uReq(my_url)
